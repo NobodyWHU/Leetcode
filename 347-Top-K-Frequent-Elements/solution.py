0532cdabb4e2c1f48aeb30bf3a10dda704bd31d2
Jeremy Wang <haohaoranran@126.com>
2016-05-17 19:50:44 +0800

    [347. Top K Frequent Elements][Accepted]committed by LeetCode Extension

--- 347-Top-K-Frequent-Elements/solution.py
+++ 347-Top-K-Frequent-Elements/solution.py
@@ -6,7 +6,13 @@ class Solution(object):
         :rtype: List[int]
         """
         from collections import Counter
+        import heapq
+        
         counter = Counter(nums)
         sorted_items = sorted(counter.items(), key=lambda x:x[1], reverse=True)
+        """
+        we can use heapq
+        heapq.nlargest(k, sorted_items, key=lambda x:c[x])
+        """
         return [item[0] for item in sorted_items[:k]]
         
