04f9d3f3a156dfd267531cdea3e85017ce45cf81
Jeremy Wang <haohaoranran@126.com>
2016-06-07 15:19:58 +0800

    [206. Reverse Linked List][Accepted]committed by LeetCode Extension

---
+++ 206-Reverse-Linked-List/solution.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        if not head.next:
+            return head
+        p, q = head, None
+        while p:
+            next = p.next
+            p.next = q
+            q = p
+            p = next
+        return q
+            
