5e9397c06adf24fae259379439b40c627617dda5
Jeremy Wang <haohaoranran@126.com>
2016-06-08 13:55:33 +0800

    [169. Majority Element][Accepted]committed by LeetCode Extension

--- 169-Majority-Element/solution.py
+++ 169-Majority-Element/solution.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums)==1:
-            return nums[0]
-        nums = sorted(nums)
-        return nums[len(nums)/2]
+        count = 1
+        majority = nums[0]
+        for i in range(1, len(nums)):
+            if majority == nums[i]:
+                count += 1
+            else:
+                if count == 0:
+                    count = 1
+                    majority = nums[i]
+                else:
+                    count -= 1
+        
+        return majority
