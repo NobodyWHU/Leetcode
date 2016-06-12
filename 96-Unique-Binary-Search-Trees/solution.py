1400c8c60bfebcd2570294db42ef8132528bdf82
Jeremy Wang <haohaoranran@126.com>
2016-06-12 17:08:51 +0800

    [96. Unique Binary Search Trees][Accepted]committed by LeetCode Extension

--- 96-Unique-Binary-Search-Trees/solution.py
+++ 96-Unique-Binary-Search-Trees/solution.py
@@ -4,11 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        ans = 0
-        for i in range(0, n):
-            ans += self.numTrees(i)*self.numTrees(n-i-1)
-        return ans
+        if n < 2:
+            return n
+        dp = [ 0 for i in range(n + 1)]
+        dp[0] = dp[1] = 1
+        for i in range(2, n+1):
+            mid = i // 2
+            for j in range(0, mid):
+                dp[i] += (dp[j] * dp[i-1-j]) * 2
+            if i % 2 == 1:
+                dp[i] += dp[mid] ** 2
+        return dp[-1]
