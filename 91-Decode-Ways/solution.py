947cfc7092d1f5dfd11236d5294d7ec8ec3971ca
Jeremy Wang <haohaoranran@126.com>
2016-06-12 19:47:14 +0800

    [91. Decode Ways][Accepted]committed by LeetCode Extension

---
+++ 91-Decode-Ways/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        dp = [0 for i in range(len(s)+1)]
+        dp[0] = 1
+        dp[1] = 0 if s[0] == "0" else 1
+        for i in range(2,len(s)+1):
+            if s[i-1] != "0":
+                dp[i] += dp[i-1]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+        return dp[len(s)] 
