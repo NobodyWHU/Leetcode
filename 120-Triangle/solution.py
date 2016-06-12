8fd9f5c719f57a39a8833a1b3d6d693c2f24298c
Jeremy Wang <haohaoranran@126.com>
2016-06-12 13:03:19 +0800

    [120. Triangle][Accepted]committed by LeetCode Extension

---
+++ 120-Triangle/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if len(triangle) == 0:
+            return 0
+        if len(triangle) == 1:
+            return triangle[0][0]
+        dp = [i for i in triangle[-1]]
+        
+        for i in range(len(triangle)-2, -1, -1):
+            for j in range(len(triangle[i])):
+                dp[j] = min(dp[j], dp[j+1]) + triangle[i][j]
+        
+        return dp[0]
+        
