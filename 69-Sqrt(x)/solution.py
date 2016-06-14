11b39d55b751a047eaa125d672efcec382c94812
Jeremy Wang <haohaoranran@126.com>
2016-06-14 13:20:43 +0800

    [69. Sqrt(x)][Accepted]committed by LeetCode Extension

---
+++ 69-Sqrt(x)/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        start, end = 1, x
+        while start + 1 < end:
+            mid = (start + end) / 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                start = mid
+            else:
+                end = mid
+        if end * end <= x:
+            return end
+        return start
