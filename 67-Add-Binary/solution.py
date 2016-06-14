849e035e81eed9d333de363d89b0f210ca1a34ac
Jeremy Wang <haohaoranran@126.com>
2016-06-14 13:26:48 +0800

    [67. Add Binary][Accepted]committed by LeetCode Extension

---
+++ 67-Add-Binary/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        res = int(a,2)+int(b,2)
+        return bin(res)[2:]
