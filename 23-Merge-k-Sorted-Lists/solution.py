a60d250c737791e3ee6f1584615bdad7c923078b
Jeremy Wang <haohaoranran@126.com>
2016-06-16 15:46:11 +0800

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

---
+++ 23-Merge-k-Sorted-Lists/solution.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        import heapq
+        heap = []
+        for node in lists:
+            if node:
+                heap.append((node.val, node))
+        
+        heapq.heapify(heap)
+        head = ListNode(0)
+        curr = head
+        while heap:
+            pop = heapq.heappop(heap)
+            curr.next = ListNode(pop[0])
+            curr = curr.next
+            if pop[1].next:
+                heapq.heappush(heap, (pop[1].next.val, pop[1].next))
+        return head.next
+        
