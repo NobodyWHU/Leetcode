f7a8eeae9f1999665a1860d43c53473656fe5472
Jeremy Wang <haohaoranran@126.com>
2016-06-16 21:36:37 +0800

    [9. Palindrome Number][Accepted]committed by LeetCode Extension

---
+++ 9-Palindrome-Number/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        s = int(str(x)[::-1])
+        return s == x
