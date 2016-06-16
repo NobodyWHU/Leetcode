60a85df27ea636f18f26e1cd91e437bb1ecd5503
Jeremy Wang <haohaoranran@126.com>
2016-06-16 15:51:59 +0800

    [22. Generate Parentheses][Accepted]committed by LeetCode Extension

---
+++ 22-Generate-Parentheses/solution.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        if n==0:
+            return []
+        res = []
+        self.helper(n,n,"",res)
+        return res
+    
+    
+    def helper(self, l, r, item, res):
+        if r < l:
+            return 
+        if l == 0 and r == 0:
+            res.append(item)
+        if l > 0:
+            self.helper(l-1, r, item+"(", res)
+        if r > 0:
+            self.helper(l, r-1, item+")", res)
+        
+        
