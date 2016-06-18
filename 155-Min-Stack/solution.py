4a162d4e413584d2999080852feefbaed872f897
Jeremy Wang <haohaoranran@126.com>
2016-06-18 22:29:35 +0800

    [155. Min Stack][Accepted]committed by LeetCode Extension

---
+++ 155-Min-Stack/solution.py
@@ -0,0 +1,49 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minstack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+        if len(self.minstack) == 0 or x <= self.minstack[-1]:
+            self.minstack.append(x)
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if self.stack[-1] == self.minstack[-1]:
+            self.minstack.pop()
+        return self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minstack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
