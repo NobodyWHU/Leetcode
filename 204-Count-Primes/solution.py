258ab620ab82c4b3bcdb2176c0aac1fca4b53fd1
Jeremy Wang <haohaoranran@126.com>
2016-06-18 17:29:56 +0800

    [204. Count Primes][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 204-Count-Primes/solution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return 0
+        c = 1
+        primeFlag = [False] * (n+1)
+        
+        i = 3
+        while i*i <= n:
+            if primeFlag[i]:
+                continue
+            j = i**2
+            while j <= n:
+                primeFlag[j] = True
+                j += 2*i
+            i = i + 2
+        
+        for i in range(3, n, 2):
+            if not primeFlag[i]:
+                c += 1
+        
+        
+        return c
