9d6d6875322aab1667e60243d7a6e250aaded881
Jeremy Wang <haohaoranran@126.com>
2016-06-28 15:40:41 +0800

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

---
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        n = len(nums)
+        left, right = 0, n-1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] == target:
+                return True
+            if nums[mid] < nums[right]:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            elif nums[mid] > nums[right]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                right -= 1
+        
+        return False
+        
