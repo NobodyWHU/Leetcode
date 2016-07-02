10bc6862febb3864e67f9b16db170388341a43f1
Jeremy Wang <haohaoranran@126.com>
2016-07-02 23:01:58 +0800

    [306. Additive Number][Accepted]committed by LeetCode Extension

---
+++ 306-Additive-Number/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isAdditiveNumber(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        from itertools import combinations
+        n = len(num)
+        for i, j in combinations(range(1, n), 2):
+            a, b = num[:i], num[i:j]
+            if a != str(int(a)) or b != str(int(b)):
+                continue
+            while j < n:
+                c = str(int(a)+int(b))
+                if not num.startswith(c, j):
+                    break
+                j += len(c)
+                a, b = b, c
+            if j == n:
+                return True
+        return False
