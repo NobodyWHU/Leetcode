7c47f72d0e85e2a1f63e231806be66923154cd29
Jeremy Wang <haohaoranran@126.com>
2016-07-03 11:26:27 +0800

    [139. Word Break][Accepted]committed by LeetCode Extension

---
+++ 139-Word-Break/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: Set[str]
+        :rtype: bool
+        """
+        dp = [False for _ in range(len(s)+1)]
+        dp[0] = True
+        for i in range(1, len(s)+1):
+            for k in range(i):
+                if dp[k] and s[k:i] in wordDict:
+                    dp[i] = True
+        return dp[len(s)]
