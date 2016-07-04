2c7d4ed8dba582d4075a24eeab0a0d009983b2c3
Jeremy Wang <haohaoranran@126.com>
2016-07-04 13:05:14 +0800

    [133. Clone Graph][Accepted]committed by LeetCode Extension

---
+++ 133-Clone-Graph/solution.py
@@ -0,0 +1,24 @@
+# Definition for a undirected graph node
+# class UndirectedGraphNode(object):
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: UndirectedGraphNode
+        :rtype: UndirectedGraphNode
+        """
+        def dfs(input, map):
+            if input in map:
+                return map[input]
+            output = UndirectedGraphNode(input.label)
+            map[input] = output
+            for n in input.neighbors:
+                output.neighbors.append(dfs(n, map))
+            return output
+        
+        if not node:
+            return None
+        return dfs(node, {})
