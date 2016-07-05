70168cf4387ca0765fe6e2386bd3ec6a54a2356c
Jeremy Wang <haohaoranran@126.com>
2016-07-05 14:33:54 +0800

    [3. Longest Substring Without Repeating Characters][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 3-Longest-Substring-Without-Repeating-Characters/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        n = len(s)
+        ans = -1
+        for i in range(n):
+            temp = set()
+            temp.add(s[i])
+            for j in range(i+1, n):
+                if s[j] in temp:
+                    break
+                else:
+                    temp.add(s[j])
+            ans = max(len(temp), ans)
+        return ans
