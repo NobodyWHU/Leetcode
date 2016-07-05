bbae5b29bfc149d7640a54634bed4c6225ea1798
Jeremy Wang <haohaoranran@126.com>
2016-07-05 14:06:50 +0800

    [152. Maximum Product Subarray][Accepted]committed by LeetCode Extension

---
+++ 152-Maximum-Product-Subarray/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        n = len(nums)
+        maxval = [0] * n
+        minval = [0] * n
+        maxval[0] = minval[0] = nums[0]
+        for i in range(1, n):
+            maxval[i] = max(maxval[i-1]*nums[i], minval[i-1]*nums[i], nums[i])
+            minval[i] = min(minval[i-1]*nums[i], maxval[i-1]*nums[i], nums[i])
+        return max(max(maxval), max(minval))
