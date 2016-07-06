4d74c63869bcb8472b4694406f492e65e2cd5136
Jeremy Wang <haohaoranran@126.com>
2016-07-06 23:41:13 +0800

    [127. Word Ladder][Accepted]committed by LeetCode Extension

---
+++ 127-Word-Ladder/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        """
+        :type beginWord: str
+        :type endWord: str
+        :type wordList: Set[str]
+        :rtype: int
+        """
+        wordList.add(endWord)
+        q = []
+        q.append((beginWord, 1))
+        while q:
+            curr = q.pop(0)
+            currword = curr[0]
+            currlen = curr[1]
+            if currword == endWord:
+                return currlen
+            for i in range(len(beginWord)):
+                part1, part2 = currword[:i], currword[i+1:]
+                for j in "abcdefghijklmnopqrstuvwxyz":
+                    if currword[i] != j:
+                        nextword = part1+j+part2
+                        if nextword in wordList:
+                            q.append((nextword, currlen+1))
+                            wordList.remove(nextword)
+        return 0
