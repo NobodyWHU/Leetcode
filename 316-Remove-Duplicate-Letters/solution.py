249ee5cac3806b6492c6a78c0759a84d83b0be42
Jeremy Wang <haohaoranran@126.com>
2016-07-13 15:36:19 +0800

    [316. Remove Duplicate Letters][Accepted]committed by LeetCode Extension

---
+++ 316-Remove-Duplicate-Letters/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        from collections import Counter
+        counter = Counter(s)
+        resultset = set()
+        stack = []
+        for c in s:
+            counter[c] -= 1
+            if c in resultset:
+                continue
+            while stack and stack[-1] > c and counter[stack[-1]]:
+                resultset.remove(stack.pop())
+            resultset.add(c)
+            stack.append(c)
+        
+        return "".join(stack)
